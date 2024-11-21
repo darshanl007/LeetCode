daad63b8bdf3af6e5af236950b043382f3274a1c
Darshan L <darshanl1254@gmail.com>
2024-11-21 12:29:59 +0530

    Leetcode 1 and 9

--- 1.java
+++ 1.java
@@ -1,4 +1,4 @@
-package Easy;
+
 class Solution {
 	public int[] twoSum(int[] nums, int target) {
 		for (int i = 0; i < nums.length; i++) {
---
+++ 9.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reverse = 0;
+        int temp = x;
+        while(temp>0){
+            int rem = temp % 10;
+            reverse = (reverse * 10) + rem;
+            temp = temp/10;
+        }
+        if(x == reverse){
+            return true;
+        }      
+        else{
+            return false;
+        }
+    }
+}
