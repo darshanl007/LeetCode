9825ee0167ab84c29efd8f2c054b0bb4e50cda41
Darshan L <darshanl1254@gmail.com>
2024-11-22 17:33:30 +0530

    Lt - 88(Merge Sorted Arrays)

---
+++ 88.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] nums3 = new int[m+n];
+        for(int i=0;i<m;i++){
+            nums3[i] = nums1[i];
+        }
+        for(int i =0; i<n;i++){
+            nums3[m+i] = nums2[i];
+        }
+        for(int i=0;i<nums3.length;i++){
+            nums1[i] = nums3[i];
+        }
+        Arrays.sort(nums1);
+    }
+}
