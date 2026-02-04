d7b53ec4cec79404b20e7fa48a6b260ee5ae1761
Darshan L <darshanl1254@gmail.com>
2026-02-04 16:04:12 +0530

    Lt - 121(Best Time to Buy and Sell Stock)

--- 121.java
+++ 121.java
@@ -1,15 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
+        int n = prices.length;
+        int max_profit = 0;
+        int buy_price = prices[0];
 
-        for (int price : prices) {
-            if (price < minPrice) {
-                minPrice = price;     
-            } else {
-                maxProfit = Math.max(maxProfit, price - minPrice);
+        for(int i = 1; i < n; i++){
+            int curr_profit = prices[i] - buy_price;
+            if(curr_profit > max_profit){
+                max_profit = curr_profit;
+            }
+            if(prices[i] < buy_price){
+                buy_price = prices[i];
             }
         }
-        return maxProfit;
+        return max_profit;
     }
 }
