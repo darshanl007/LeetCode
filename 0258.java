fb080c3087c2478ef32a80597c3f135eb480afbb
Darshan L <darshanl1254@gmail.com>
2026-02-09 16:00:30 +0530

    Lt - 258(Add Digits)

--- 0258.java
+++ 0258.java
@@ -1,14 +1,7 @@
 class Solution {
     public int addDigits(int num) {
-        while(num > 9){
-            int sum = 0;
-            while(num > 0){
-                int rem = num % 10;
-                sum = sum + rem;
-                num = num / 10;
-            }
-            num = sum;
-        }
-        return num;
+        if(num == 0) return 0;
+        if(num % 9 == 0) return 9;
+        return num % 9;
     }
 }
